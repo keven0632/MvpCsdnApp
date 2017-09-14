8e7741a50b2600bfdf96b8e6b348f81735c627a8
zhengjian <jian.zheng@cihon.cn>
2017-09-14 16:59:37 +0800

    代码提交20170914

---
+++ app/src/main/java/keven/cihon/mvpcsdnapp/book/BooksFragment.java
@@ -0,0 +1,30 @@
+package keven.cihon.mvpcsdnapp.book;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import keven.cihon.mvpcsdnapp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BooksFragment extends Fragment {
+
+
+    public BooksFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_books, container, false);
+    }
+
+}
